2a73e7a6f41feff524d45d8060da6fcfa9194195
darchik <darchik88@gmail.com>
2020-09-16 01:32:01 -0400

    commiting stylings for product detail header, price with and without discount

--- public/components/PriceComponent.jsx
+++ public/components/PriceComponent.jsx
@@ -1,13 +1,53 @@
+/* eslint-disable no-mixed-operators */
+/* eslint-disable max-len */
+/* eslint-disable radix */
 /* eslint-disable react/prop-types */
 /* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable import/extensions */
 import React from 'react';
+import PriceBeforeDiscount from './PriceBeforeDiscount.jsx';
 
-const PriceComponent = ({ price, seller, shippingOptions }) => (
-  <div>
-    <div>{price}</div>
-    <div>{seller}</div>
-    <div>{shippingOptions}</div>
-  </div>
-);
+const PriceComponent = ({ price, discount, shippingOptions }) => {
+  const parsed = parseInt(price).toFixed(2);
+  return (
+    <div id="product-vitals" className="product-vitals">
+      <div id="pricing">
+        <ul>
+          {discount === 0 ? '' : <PriceBeforeDiscount priceBefore={parsed} />}
+          <li className="our-price">
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <p className="title">
+                      {'                    Price:'}
+                    </p>
+                  </td>
+                  <td>
+                    <li className="price">
+                      {discount === 0 ? parsed : (parsed - (parsed / 100 * discount)).toFixed(2)}
+                    </li>
+                  </td>
+                  <td>
+                    <li className="free-shipping">
+                      FREE 1-3 DAY
+                      <div>
+                        SHIPPING OVER $49
+                      </div>
+                    </li>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </li>
+          <hr />
+        </ul>
+      </div>
+      <div id="shipping-options">
+        {shippingOptions}
+      </div>
+    </div>
+  );
+};
 
 export default PriceComponent;
