24da7bd79fdfa618aa4b57c685d3210adfb614c3
Dominic Silvia <dominic.silvia@gmail.com>
2020-10-07 20:05:53 -0700

    complete data generation script

--- database/data-generation/dataGenerator.js
+++ database/data-generation/dataGenerator.js
@@ -4,17 +4,20 @@ const faker = require('faker');
 
 // define output files
 const productDetailHeader = path.join(__dirname, 'output', 'product-detail-header.csv');
+const productDetailSecondary = path.join(__dirname, 'output', 'product-detail-secondary.csv');
 
-let headerCounter = 1;
-let writerOk = true;
-
-// Create writestream object
+// Create writestream object for header data
 const productHeaderStream = fs.createWriteStream(productDetailHeader);
-// Write header line of CSV file
-productHeaderStream.write('id,brand,name,seller\n');
+// Create writestream object for secondary data
+const productSecondaryStream = fs.createWriteStream(productDetailSecondary);
+
+// set counters for header and secondary records
+let headerCounter = 1;
+let secondaryCounter = 1;
 
 const generateHeaderData = () => {
-  while (headerCounter <= 1000 && writerOk) {
+  let writerOk = true;
+  while (headerCounter <= 10000000 && writerOk) {
     // create a test record
     const productBrand = faker.company.companyName();
     const productName = faker.commerce.productName();
@@ -26,11 +29,39 @@ const generateHeaderData = () => {
     // test for high watermark
   }
   if (!writerOk) {
-    // if hit, drain the buffer, then continue iterating
-    console.log('HIGHWATERMARK HIT');
+    // Drain the buffer, then continue iterating
     productHeaderStream.once('drain', generateHeaderData);
   }
-  // write last record and call secondary record generator
 };
 
+const generateSecondaryData = () => {
+  let writerOk = true;
+  while (secondaryCounter <= 10000000 && writerOk) {
+    // create 3 secondary records for each product
+    const price = faker.commerce.price(3, 120);
+    // eslint-disable-next-line no-loop-func
+    [['small', 1], ['medium', 2], ['large', 3]].forEach((size) => {
+      const discount = faker.random.boolean() ? 5 : 0;
+      const shippingOptions = faker.random.boolean() ? '2 day shipping' : '';
+      const itemStock = faker.random.number(10);
+      const isFavorite = false;
+      writerOk = productSecondaryStream.write(`${secondaryCounter},${size[0]},${price * size[1]},${discount},${shippingOptions},${itemStock},${isFavorite}
+      `);
+    });
+    secondaryCounter += 1;
+  }
+
+  if (!writerOk) {
+    // Drain the buffer, then continue iterating
+    productSecondaryStream.once('drain', generateSecondaryData);
+  }
+};
+
+// Write header line of CSV file
+productHeaderStream.write('id,brand,name,seller\n');
+// Generate Header Data
 generateHeaderData();
+
+// Write header line of CSV file
+productSecondaryStream.write('id,size,price,discount,shipping_options,item_stock,is_favorite\n');
+generateSecondaryData();
